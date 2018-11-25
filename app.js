2708e46b62993efd7ddc427a44a1c315240cbc62
Usman Akram <usman.akram1996@outlook.com>
2018-11-25 22:29:02 +0500

    Express Sessions Part 2(i)

--- app.js
+++ app.js
@@ -47,39 +47,28 @@ app.use(session({
 }))
 // app.use(cookieParser('12345-67890-09876-54321'));
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 function auth (req, res, next) {
 
   console.log(req.session);
 
   if (!req.session.user) {
-    var authHeader = req.headers.authorization;
-    if (!authHeader) {
+    // var authHeader = req.headers.authorization;
+    // if (!authHeader) {
         var err = new Error('You are not authenticated!');
         res.setHeader('WWW-Authenticate', 'Basic');              
         err.status = 401;
-        next(err);
-        return;
-    }
-    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-    var user = auth[0];
-    var pass = auth[1];
-    if (user == 'admin' && pass == 'password') {
-        req.session.user = 'admin';
-        next(); // authorized
-    } else {
-        var err = new Error('You are not authenticated!');
-        res.setHeader('WWW-Authenticate', 'Basic');              
-        err.status = 401;
-        next(err);
-    }
-  }
+        return next(err);
+  }        
   else {
-      if (req.session.user === 'admin') {
+      if (req.session.user === 'authenticated') {
           next();
       }
       else {
           var err = new Error('You are not authenticated!');
-          err.status = 401;
+          err.status = 403 ;
           next(err);
       }
   }
@@ -90,8 +79,6 @@ app.use(auth);
 // Here the user is allowed to access the data after authentication
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
