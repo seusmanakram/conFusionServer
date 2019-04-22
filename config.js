67ebb6b3669fd406ff2f4140918b65b7cf0a9f1f
Usman Akram <usman.akram1996@outlook.com>
2019-04-22 16:42:39 +0500

    Passport Facebook

--- config.js
+++ config.js
@@ -2,6 +2,11 @@
 
 module.exports = {
     'secretKey' : '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: 2358731487690861 ,
+        clientSecret: '694f2d07030b24bad44fb91c86c17e3e'
+    }
+
 
 }
